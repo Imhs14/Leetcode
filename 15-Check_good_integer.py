217f2df680ba21b0e775c6654f4a9ec5815397bd
imhs14 <heerashanker0214@gmail.com>
2026-06-20 22:37:53 +0530

    day 11 check good integer

---
+++ 15-Check_good_integer.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkGoodInteger(self, n: int) -> bool:
+        dig_sum, sq_sum = 0, 0
+        for i in str(n):
+            a = n %10
+            p = n // 10
+            n = p
+            dig_sum += a
+            sq_sum += a*a
+        
+        return  sq_sum - dig_sum >= 50 
