a8b3f462d90d577af77f0291818a66b0762c3788
imhs14 <heerashanker0214@gmail.com>
2026-06-22 03:05:53 +0530

    Two sum

--- 5-Two_sum.py
+++ 5-Two_sum.py
@@ -7,4 +7,10 @@
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
+        seen = {nums[0]: 0}
+        for i in range(1,len(nums)):
+            a = target - nums[i]
+            if a in seen.keys() and (i != seen[a]):
+                return [i,seen[a]]
+            
+            seen[nums[i]] = i 
