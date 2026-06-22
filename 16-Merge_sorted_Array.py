4cea912410b17094c59c1efee92a8d0d8c015707
imhs14 <heerashanker0214@gmail.com>
2026-06-23 00:36:33 +0530

    updated no. questions solved

---
+++ 16-Merge_sorted_Array.py
@@ -0,0 +1,14 @@
+# LeetCode Problem N/A: Merge Sorted Array
+# URL: https://leetcode.com/problems/merge-sorted-array/?envType=problem-list-v2&envId=wpb3lnsi
+# Difficulty: Easy
+# Category: Two Pointer
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        j = 0
+        for i in range(m,len(nums1)):
+            nums1[i] = nums2[j]
+            j += 1
+        nums1.sort()
