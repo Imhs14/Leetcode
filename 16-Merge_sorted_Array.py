f2b08b668c7b31a30e3d8a62819e0aca9e858f22
imhs14 <heerashanker0214@gmail.com>
2026-06-23 00:42:41 +0530

    updated 1st try

--- 16-Merge_sorted_Array.py
+++ 16-Merge_sorted_Array.py
@@ -12,3 +12,16 @@ class Solution:
             nums1[i] = nums2[j]
             j += 1
         nums1.sort()
+
+"""
+raw and unoptimized solution
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        j,k = 0,1
+        for i in range(m+1,m+n+1):
+            nums1[m+k-1] = nums2[j]
+            j += 1
+            k += 1
+        
+        nums1.sort()    
+"""
