d1d312a59f68d35999aeba7b1f858fc14bbcb01b
imhs14 <heerashanker0214@gmail.com>
2026-06-24 04:25:35 +0530

    771 jewels and stones completed

---
+++ 18-Jewels_and_Stones.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        for i in range(0,len(jewels)):
+            j = 0
+            while j < len(stones):
+                if jewels[i] == stones[j]:
+                    count += 1
+                    j += 1
+                else:
+                    j += 1
+            
+        return count
+    
+# Above solution having Time = O(m + n) , Space = O(1)
+
+"""
+# Time = O(JxS) Space = O(1)
+count = 0
+for i in range(0,len(jewels)):
+    j = 0
+    while j < len(stones):
+        if jewels[i] == stones[j]:
+            count += 1
+            j += 1
+        else:
+            j += 1
+
+print(count)
+    """
