af4aefa609aaf993a30ae35c8930c7122e91cfb1
M Heera Shanker <heerashanker0214@gmail.com>
2026-06-25 10:20:31 +0530

    Update 21-Moves_Zeros.py

--- 21-Moves_Zeros.py
+++ 21-Moves_Zeros.py
@@ -6,4 +6,20 @@ class Solution:
                 nums[L], nums[r] = nums[r], nums[L]
                 L += 1
         # Time = O(n) , Space = O,(1), # Two pointers
+"""
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        k,j = 0,0
+        for i in range(0, len(nums)):
+            if nums[i] != 0:
+                 nums[k]= nums[i]
+                 k += 1
+            else:
+                j+=1
+    
+        while j > 0:
+           nums[-j] = 0
+           j-=1  
+        # Time = O(n) , Space = O,(1), # Two pointers
+"""
     
