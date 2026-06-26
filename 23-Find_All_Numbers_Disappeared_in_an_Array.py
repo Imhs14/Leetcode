94fbc38bd5387790a2aad1badc42069e763239c0
imhs14 <heerashanker0214@gmail.com>
2026-06-26 23:18:45 +0530

    find all the numbers that disappeared in an array

--- 23-Find_All_Numbers_Disappeared_in_an_Array.py
+++ 23-Find_All_Numbers_Disappeared_in_an_Array.py
@@ -1,6 +1,28 @@
-nums = [4,3,2,7,8,2,3,1]
-res = {}
-m = range(max(nums))
-for i in range(1,max(nums)):
-    res[i]
-    
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        return list(set(range(1, len(nums)+1))-set(nums))
+
+"""
+My other trys
+case : 1
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        z = set(range(1, len(nums)+1))
+        y = set(nums)
+        
+        return list(z-y)
+        
+case : 2
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        result = []
+        for num in nums:
+            idx = abs(num) - 1
+            if nums[idx] > 0:
+                nums[idx] *= -1
+
+        # Loop 2: find indices that are still positive
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                result.append(i + 1)
+        return result"""
