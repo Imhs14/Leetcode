26b326d4aad3f13db221befc4084ab4e92ef1a74
imhs14 <heerashanker0214@gmail.com>
2026-06-29 02:20:25 +0530

    217 contains duplicates done

---
+++ 24-Contains_Duplicate.py
@@ -0,0 +1,28 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for num in nums:
+            if num not in s :
+                s.add(num)
+                res = False
+            else:
+                return True # even if we detected a 1 duplicate just return True 
+        return res
+    # Time , Space =  O(n)
+
+
+"""
+Another way to solve it 
+
+s = set()
+for num in nums:
+    if num in s:
+        return True
+    else:
+        return False
+return False
+
+Another way to solve it but not efficient, in this we do not have an early exit 
+
+return (len(nums) != len(set(nums))) 
+"""
