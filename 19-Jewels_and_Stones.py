3130d4c139dde2b26f5d7db60d9631a6d141de5f
imhs14 <heerashanker0214@gmail.com>
2026-06-29 01:09:53 +0530

    jewels and stones done

--- 19-Jewels_and_Stones.py
+++ 19-Jewels_and_Stones.py
@@ -1,20 +1,34 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         count = 0
-        for i in range(0,len(jewels)):
-            j = 0
-            while j < len(stones):
-                if jewels[i] == stones[j]:
-                    count += 1
-                    j += 1
-                else:
-                    j += 1
-            
+        dic1 = {}
+        for i,char in enumerate(jewels):
+            dic1[char] = i
+
+        for j in range(0, len(stones)):
+            if stones[j] in dic1.keys():
+                count += 1
+
         return count
     
 # Above solution having Time = O(m + n) , Space = O(1)
 
 """
+# Time = O(m + n), space = O(1)
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        s = set(jewels)
+        count = 0
+        for stone in stones:
+            if stone in jewels:
+                count += 1
+        
+        return count 
+
+# this is better and the solution uses the hash sets
+
+
 # Time = O(JxS) Space = O(1)
 count = 0
 for i in range(0,len(jewels)):
