ef818269cb777e7c1ef2263746da5b70114d8269
imhs14 <heerashanker0214@gmail.com>
2026-06-29 23:10:19 +0530

    383 ransomNote done

---
+++ 25-RansomNote.py
@@ -0,0 +1,54 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counter = {}
+        for c in magazine:
+            if c in counter:
+                counter[c] += 1
+            else:
+                counter[c] = 1
+            
+        for c in ransomNote:
+
+            if c not in counter:
+                return False
+            elif counter[c] == 1:
+                del counter[c]
+            else:
+                counter[c] -= 1
+
+        return True 
+
+"""
+Alternate ways of solving it
+case : 1
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        letters = Counter(magazine)
+        for letter in ransomNote:
+            if letters.get(letter, 0) < 1:
+                return False
+            letters[letter] -= 1
+        return True
+
+case : 2
+from collections import Counter
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for char in set(ransomNote):
+            if ransomNote.count(char) > magazine.count(char):
+                return False
+        return True
+
+case : 3
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        hashmap = Counter(magazine) # TC for Counter is O(n)
+ 
+        for ch in ransomNote:
+            if hashmap[ch] > 0:
+                hashmap[ch]-=1
+            else:
+                return False
+        return True
+""" 
