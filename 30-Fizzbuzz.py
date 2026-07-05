7e6a2231ae5d86596a7a47544ab9e4fbe10cd540
imhs14 <heerashanker0214@gmail.com>
2026-07-06 02:26:36 +0530

    441 Fizzbuzz done

---
+++ 30-Fizzbuzz.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        op = []
+        for n in range(1, n+1):
+            if n % 3 == 0 and n % 5 == 0:
+                op.append("FizzBuzz")
+            elif n % 3 == 0:
+                op.append("Fizz")
+            elif n % 5 == 0 :
+                op.append("Buzz")
+            else:
+                op.append(str(n))
+        return op
