aaee209b17fec62ba66045de3cc469f7b233270c
imhs14 <heerashanker0214@gmail.com>
2026-07-07 05:05:46 +0530

    shuffle the array, valid suduko, group anagrams done

---
+++ 31-Shuffle_the_Array.py
@@ -0,0 +1,28 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+        for i in range(0, n):
+            res.extend([nums[i], nums[n+i]])
+        return res
+
+# Time = O(n), Space = O(n)
+
+# case 1 
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+        i = 0 
+        while i < n:
+            res.append(nums[i])
+            res.append(nums[n+i])
+            i += 1
+        return res
+    
+# case 2
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+        for i in range(0, n):
+            res.append(nums[i])
+            res.append(nums[n+i])
+        return res
