391c4bd2e9a837c1c3eec9f89d37b971dc69be2c
imhs14 <heerashanker0214@gmail.com>
2026-07-15 01:02:16 +0530

    solution to find the maximum number of words in a sentence

--- 38-Max_num_of_words_found_in_a_sentence.py
+++ 38-Max_num_of_words_found_in_a_sentence.py
@@ -1,2 +1,14 @@
-sentence = ["alice and bob love leetcode","i think so too","this is great thanks very much"]
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        mcx = 0
+        for sentence in sentences:
+            c = len(sentence.split())
+            if c > mcx:
+                mcx = c
+        return mcx
+# Time = O(n), Space = O(1) -> as we are storing only mcx which is only 1 charater integer. 
 
+"""
+sentences = ["alice and bob love leetcode","i think so too","this is great thanks very much"]
+Output = 6
+"""
