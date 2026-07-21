6f478fbbf5be927ad14cee28f6e804689b77f88a
imhs14 <heerashanker0214@gmail.com>
2026-07-22 00:12:35 +0530

    Add alternative twoSum solution using two-pointer technique

--- 44-Two_sum_II- Input_Array_is_sorted.py
+++ 44-Two_sum_II- Input_Array_is_sorted.py
@@ -10,3 +10,19 @@ class Solution:
             seen[numbers[i]] = i
         
 # Time = O(n), Space = O(n)
+
+# Other ways to solve it
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        base = 0
+        tail = len(numbers)-1
+
+        while base < tail:
+            if numbers[base] + numbers[tail] == target:
+                return [base + 1, tail +1]
+            diff = numbers[base] + numbers[tail] - target
+            if diff < 0:
+                base += 1
+            if diff >0:
+                tail -= 1
+# Time = O(n), Space = O(1)\
