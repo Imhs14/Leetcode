86b2de21011bc492f9d0dd67f4a1d0f176aba85f
imhs14 <heerashanker0214@gmail.com>
2026-07-24 19:23:56 +0530

    46- 3sum done

--- 44-Two_sum_II- Input_Array_is_sorted.py
+++ 44-Two_sum_II- Input_Array_is_sorted.py
@@ -25,4 +25,23 @@ class Solution:
                 base += 1
             if diff >0:
                 tail -= 1
-# Time = O(n), Space = O(1)\
+# Time = O(n), Space = O(1)
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+        n = len(numbers)
+        l = 0
+        r = n -1
+
+        while l < r:
+            sum = numbers[l] + numbers[r]
+            if sum == target:
+                return [l + 1, r + 1]
+            elif sum < target:
+                l += 1
+            else:
+                r -= 1
+
+# Time = O(n)
+# Space = O(1)
