b60109710128125f21b712411ca202ad088f62ed
imhs14 <heerashanker0214@gmail.com>
2026-08-01 22:31:33 +0530

    52 question done

---
+++ 52-Count_the_digits_that_divide_a_number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countDigits(self, num: int) -> int:
+        a = str(num)
+        count = 0
+        for i in range(0,len(a)):
+            if num % int(a[i]) == 0:
+                count += 1
+        return count
+
+class Solution:
+    def countDigits(self, num: int) -> int:
+        count = 0
+        n = num
+        while n > 0:
+            digit = n % 10
+            if num % digit == 0:
+                count += 1
+            n = n // 10
+        return count
+
+# Both Follows Time = O(n), Space = O(1)
