a9d5e9cfbdb8db4ee285b180ff7be7e12575d3f6
imhs14 <heerashanker0214@gmail.com>
2026-08-03 13:37:50 +0530

    gfg's strong numbers

--- 49-Pascals_Triangle.py
+++ 49-Pascals_Triangle.py
@@ -1,3 +1,4 @@
 #def generate(self, numRows: int) -> List[List[int]]:
 s = [[1,0],[2,3,3],[9,0,8]]
 print(s[1][1])
+# needs to be solved
---
+++ 53-Strong_numbers.py
@@ -0,0 +1,19 @@
+import math
+class Solution:
+    def isStrong(self, n):
+        # code here
+        s = str(n)
+        v = 0
+        for i in range(0,len(s)):
+            z = math.factorial(int(s[i]))
+            v += z
+
+        return v==n
+
+"""
+input = 145
+Output: true
+
+Explanation: The sum of the factorials of its digits is: 1! + 4! + 5! = 1 + 24 + 120 = 145.
+Since the sum equals the original number, 145 is a Strong Number
+"""
