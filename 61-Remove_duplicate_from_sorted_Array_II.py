58656b69e6f6cc55d5c5798ced24649ad1463a4b
imhs14 <heerashanker0214@gmail.com>
2026-08-14 15:43:48 +0530

    update

---
+++ 61-Remove_duplicate_from_sorted_Array_II.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 2
+        for i in range(2,len(nums)):
+            if nums[i] != nums[k - 2]:
+                nums[k] = nums[i]
+                k += 1
+        return k
+
+"""
+for at most k values k = should be that at most number(req same number of repetation after n duplicates)
+
+for above case we needed at most 2 duplicates so we took, k = 2, started out loop at 2.
+as the loops are sorted
+"""
