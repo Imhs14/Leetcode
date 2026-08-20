f86b372cee8b28946c1bbbd514e23c24ab6dc559
imhs14 <heerashanker0214@gmail.com>
2026-08-20 18:36:29 +0530

    rotate 90 degrees

---
+++ 63-Rotate-90.py
@@ -0,0 +1,12 @@
+matrix = [[0,1,2],[3,4,5],[6,7,8]]
+n = len(matrix)
+print(matrix)
+for i in range(len(matrix)-1,-1,-1):
+    for j in range(i+1,len(matrix)):
+        matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+print(matrix)
+
+for i in range(n//2):
+    for j in range(n):
+        matrix[i][j],matrix[n - i - 1][j] = matrix[n - i - 1][j],matrix[i][j]
+print(matrix)
