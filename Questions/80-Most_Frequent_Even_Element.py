0e793a876c7d03eff97cbc195b480791d3b4779f
imhs14 <heerashanker0214@gmail.com>
2026-09-16 00:47:33 +0530

    new question updated

---
+++ Questions/80-Most_Frequent_Even_Element.py
@@ -0,0 +1,8 @@
+# Question : 2404. Most Frequent Even Element
+# Complexity : Time = O(), Space = O()
+# Topi/Category = Mid Level,Array,Hash Table,Counting,Weekly Contest 310
+# Level : Easy
+
+class Solution:
+    def mostFrequentEven(self, nums: List[int]) -> int:
+        
